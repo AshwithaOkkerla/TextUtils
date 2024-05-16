07cb76f5195b9a06e8a5bb59caf3a0f4d97d4ce8
AshwithaOkkerla <ashwitha1101@gmail.com>
2024-05-16 14:05:37 +0530

    removed route part for hoisting

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import TextForm from "./components/TextForm";
 import Navbar from "./components/Navbar";
 import { useState } from "react";
 import Alert from "./components/Alert";
-import About from "./components/About";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import About from "./components/About";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -30,24 +30,29 @@ function App() {
   };
   return (
     <>
-      <Router>
+      {/* <Router> */}
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
-          <Routes>
-            <Route exact path="/about" element={<About />}>
+          {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />}> */}
               
-            </Route>
-            <Route exact path="/" element={<TextForm
+            {/* </Route> */}
+            {/* <Route exact path="/" element={<TextForm
                 heading="Enter the text"
                 mode={mode}
                 showAlert={handleAlert}
-              />}>
+              />}> */}
               
-            </Route>
-          </Routes>
+            {/* </Route>
+          </Routes> */}
+          <TextForm
+                heading="Enter the text"
+                mode={mode}
+                showAlert={handleAlert}
+              />
         </div>
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
